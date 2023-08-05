ca93cd928598777200f447ad241080a1516163ab
james collier <astonjames@iahni.com>
2023-08-05 02:52:31 -0600

    created a product array

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
 fetch('data.json')
 .then(response =>response.json())
 .then(data =>{
-    var productList =
+    var clothingList =
         document.getElementById('clothingList');
     var 
     products = data.products;
 
-    products.forEach(product => {
+    products.forEach(products => {
         var listItem = document.createElement('li');
         var nameElement =
         document.createElement('h2');
@@ -14,7 +14,15 @@ fetch('data.json')
         document.createElement('p');
         var descriptionElement =
         document.createElement('p');
+        nameElement.textContent= products.name;
+        priceElement.textContent = 'Price: $'+ 
+        products.price.toFixed(2);
+        descriptionElement.textContent = products.description;
 
-        
+        listItem.appendChild(nameElement);
+        listItem.appendChild(priceElement);
+        listItem.appendChild(descriptionElement);
+        clothingList.appendChild(listItem);
 });
 });
+
